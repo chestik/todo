e83cecbabcf3cb2da0f417c283cbec01315b18d4
chestik <chstrwow@yandex.ru>
2021-05-24 22:38:38 +0300

    Добавление сущностей для todo и минимальная инициализация сервера в main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,13 @@
-package cmd
+package main
+
+import (
+	"log"
+	"rest_todo"
+)
+
+func main() {
+	srv := new(rest_todo.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}

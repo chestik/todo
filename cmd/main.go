2453cae2daa3fe67f5061e3bf808ab11e02f5cb1
chestik <chstrwow@yandex.ru>
2021-05-25 00:53:11 +0300

    фикс сервера

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"rest_todo"
+	"rest_todo/pkg/handler"
 )
 
 func main() {
 	srv := new(rest_todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	handlers := new(handler.Handler)
+
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }

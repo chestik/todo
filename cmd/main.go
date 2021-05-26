46a3b6879c82c8edf314eee970b353e0ccb37b3d
chestik <chstrwow@yandex.ru>
2021-05-27 00:17:27 +0300

    Добавление интерфесов сервисов, репозиториев

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	"rest_todo"
 	"rest_todo/pkg/handler"
+	"rest_todo/pkg/repository"
+	"rest_todo/pkg/service"
 )
 
 func main() {
-	srv := new(rest_todo.Server)
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(rest_todo.Server)
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
